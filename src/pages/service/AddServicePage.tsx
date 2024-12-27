d34fc6c721d49d4f7575b1c47a562bb4ce4609fd
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-12-28 01:02:28 +0630

    fix: refactor AddServicePage

--- src/pages/service/AddServicePage.tsx
+++ src/pages/service/AddServicePage.tsx
@@ -1,9 +1,7 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { Field } from "@/components/ui/field";
 import { NumberInputField } from "@/components/ui/number-input";
-import {
-  ServiceProps,
-} from "@/features/wordpress/service.service";
+import { ServiceProps } from "@/features/wordpress/service.service";
 import {
   Box,
   Button,
@@ -22,53 +20,270 @@ import {
   Text,
   Textarea,
 } from "@chakra-ui/react";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import {
+  Control,
+  Controller,
+  SubmitHandler,
+  useFieldArray,
+  useForm,
+} from "react-hook-form";
 import {
   LuDollarSign,
   LuEllipsisVertical,
   LuPencil,
   LuTrash2,
+  LuX,
 } from "react-icons/lu";
 import { v4 as uuidv4 } from "uuid";
 // import { useNavigate } from "react-router";
 import { useState } from "react";
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
-export default function AddPeoplePage() {
-  // const navigate = useNavigate();
+const FeesEditor = ({ control }: { control: Control<ServiceProps> }) => {
+  const { fields, append, remove, update } = useFieldArray({
+    control: control,
+    name: "fees",
+  });
 
-  const { register, handleSubmit, control, watch, getValues, setValue } =
-    useForm<ServiceProps>({
-      defaultValues: {
-        id: "",
-        provider: "",
-        name: "",
-        description: "",
-        fees: [],
-        ending: "",
-        indexNumber: 0,
-        visibility: false,
-      },
-    });
-
-  const [feeInput, setFeeInput] = useState<{
-    type: string;
-    amount: number;
-    description: string;
-  }>({
+  const initialFeeInputs = {
     type: "",
     amount: 0,
     description: "",
-  });
+  };
+  const [feeInputs, setFeeInputs] = useState<{
+    type: string;
+    amount: number;
+    description: string;
+  }>(initialFeeInputs);
 
-  const handleFeeInput = () => {
-    const fees = getValues("fees");
-    setValue("fees", [...fees, feeInput]);
-    setFeeInput({
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const initialEditingState = {
+    isEditing: false,
+    index: -1,
+  };
+  const [editingState, setEditingState] = useState(initialEditingState);
+
+  function syncFeeInputs(index: number) {
+    const inputs = fields[index];
+    setFeeInputs(inputs);
+  }
+
+  const handleFeeEditBtn = (index: number) => {
+    if (
+      feeInputs.type !== "" ||
+      feeInputs.amount !== 0 ||
+      feeInputs.description !== ""
+    ) {
+      setIsDialogOpen(true);
+    } else {
+      setEditingState({ isEditing: true, index });
+      syncFeeInputs(index);
+    }
+  };
+
+  const handleCancelEditBtn = () => {
+    setEditingState(initialEditingState);
+    setFeeInputs(initialFeeInputs);
+  };
+
+  const handleFeeDeleteBtn = (index: number) => {
+    remove(index);
+  };
+
+  const handleFeeInputsAddOrEditBtn = () => {
+    if (editingState.isEditing) {
+      update(editingState.index, feeInputs);
+      setEditingState(initialEditingState);
+      setFeeInputs(initialFeeInputs);
+      return;
+    }
+
+    append(feeInputs);
+    setFeeInputs({
       type: "",
       amount: 0,
       description: "",
     });
   };
+
+  return (
+    <>
+      <Flex gap={8} flexDir={{ base: "column", lg: "row" }}>
+        <Flex flexBasis={"1/2"}>
+          <Fieldset.Root
+            onKeyDown={(e) => e.key == "Enter" && console.log("Entered")}
+          >
+            <Field required label="Type">
+              <Input
+                value={feeInputs.type}
+                onChange={(e) =>
+                  setFeeInputs((prev) => {
+                    return {
+                      type: e.target.value,
+                      amount: prev.amount,
+                      description: prev.description,
+                    };
+                  })
+                }
+              />
+            </Field>
+            <Field required label="Amount">
+              <NumberInputRoot min={0}>
+                <NumberInputField
+                  value={feeInputs.amount}
+                  onChange={(e) =>
+                    setFeeInputs((prev) => {
+                      return {
+                        type: prev.type,
+                        amount: parseFloat(e.target.value),
+                        description: prev.description,
+                      };
+                    })
+                  }
+                />
+              </NumberInputRoot>
+            </Field>
+            <Field label="Description">
+              <Textarea
+                rows={5}
+                value={feeInputs.description}
+                onChange={(e) =>
+                  setFeeInputs((prev) => {
+                    return {
+                      type: prev.type,
+                      amount: prev.amount,
+                      description: e.target.value,
+                    };
+                  })
+                }
+              />
+            </Field>
+            <Button onClick={handleFeeInputsAddOrEditBtn}>
+              {editingState.isEditing ? "Editing done" : "Add new fee"}
+            </Button>
+          </Fieldset.Root>
+        </Flex>
+
+        <Flex flexBasis={"1/2"}>
+          <SimpleGrid
+            width={"100%"}
+            height={"min-content"}
+            gap={4}
+            flexDir={{ base: "column", lg: "row" }}
+          >
+            {fields.map((service, index) => (
+              <Card.Root key={index} flexGrow={1}>
+                <Card.Body>
+                  <Flex
+                    justifyContent={"space-between"}
+                    alignItems={"center"}
+                    gapX={4}
+                  >
+                    <Text fontSize={"lg"} fontWeight={"medium"}>
+                      {service.type}
+                    </Text>
+
+                    <MenuRoot>
+                      <MenuTrigger asChild>
+                        <IconButton size={"sm"} variant={"outline"}>
+                          <LuEllipsisVertical />
+                        </IconButton>
+                      </MenuTrigger>
+                      <MenuContent position={"absolute"} top={"68px"} right={6}>
+                        {editingState.index != index ? (
+                          <MenuItem
+                            value="editBtn"
+                            justifyContent={"space-between"}
+                            onClick={() => handleFeeEditBtn(index)}
+                          >
+                            Edit
+                            <LuPencil />
+                          </MenuItem>
+                        ) : (
+                          <MenuItem
+                            value="editCancelBtn"
+                            justifyContent={"space-between"}
+                            onClick={handleCancelEditBtn}
+                          >
+                            Cancel
+                            <LuX />
+                          </MenuItem>
+                        )}
+                        <MenuItem
+                          value="deleteBtn"
+                          justifyContent={"space-between"}
+                          onClick={() => handleFeeDeleteBtn(index)}
+                        >
+                          Delete
+                          <LuTrash2 />
+                        </MenuItem>
+                      </MenuContent>
+                    </MenuRoot>
+                  </Flex>
+                  <Text display={"flex"} alignItems={"center"}>
+                    <LuDollarSign />
+                    {Intl.NumberFormat().format(service.amount)}
+                  </Text>
+                  <Text mt={4} fontSize={"sm"}>
+                    {service.description}
+                  </Text>
+                </Card.Body>
+              </Card.Root>
+            ))}
+          </SimpleGrid>
+        </Flex>
+      </Flex>
+
+      <DialogRoot lazyMount role="alertdialog" open={isDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+          </DialogHeader>
+          <DialogBody>
+            <p>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our systems.
+            </p>
+          </DialogBody>
+          <DialogFooter>
+            <DialogActionTrigger asChild>
+              <Button variant="outline" onClick={() => setIsDialogOpen(false)}>
+                Cancel
+              </Button>
+            </DialogActionTrigger>
+            <Button colorPalette="red">Delete</Button>
+          </DialogFooter>
+          {/* <DialogCloseTrigger /> */}
+        </DialogContent>
+      </DialogRoot>
+    </>
+  );
+};
+
+export default function AddPeoplePage() {
+  // const navigate = useNavigate();
+
+  const { register, handleSubmit, control } = useForm<ServiceProps>({
+    defaultValues: {
+      id: "",
+      provider: "",
+      name: "",
+      description: "",
+      fees: [],
+      ending: "",
+      indexNumber: 0,
+      visibility: false,
+    },
+  });
 
   const handleSaveBtn: SubmitHandler<ServiceProps> = async (service) => {
     service.id = uuidv4();
@@ -117,119 +332,7 @@ export default function AddPeoplePage() {
         </Field>
 
         <Heading size={"2xl"}>Fees</Heading>
-        <Flex gap={8} flexDir={{ base: "column", lg: "row" }}>
-          <Flex flexBasis={"1/2"}>
-            <Fieldset.Root
-              onKeyDown={(e) => e.key == "Enter" && console.log("Entered")}
-            >
-              <Field required label="Type">
-                <Input value={feeInput.type}
-                  onChange={(e) =>
-                    setFeeInput((prev) => {
-                      return {
-                        type: e.target.value,
-                        amount: prev.amount,
-                        description: prev.description,
-                      };
-                    })
-                  }
-                />
-              </Field>
-              <Field required label="Amount">
-                <NumberInputRoot min={0}>
-                  <NumberInputField
-                  value={feeInput.amount}
-                    onChange={(e) =>
-                      setFeeInput((prev) => {
-                        return {
-                          type: prev.type,
-                          amount: parseFloat(e.target.value),
-                          description: prev.description,
-                        };
-                      })
-                    }
-                  />
-                </NumberInputRoot>
-              </Field>
-              <Field label="Description">
-                <Textarea
-                  rows={5}
-                  value={feeInput.description}
-                  onChange={(e) =>
-                    setFeeInput((prev) => {
-                      return {
-                        type: prev.type,
-                        amount: prev.amount,
-                        description: e.target.value,
-                      };
-                    })
-                  }
-                />
-              </Field>
-              <Button onClick={handleFeeInput}>Add to fees</Button>
-            </Fieldset.Root>
-          </Flex>
-
-          <Flex flexBasis={"1/2"}>
-            <SimpleGrid
-              width={"100%"}
-              height={"min-content"}
-              gap={4}
-              flexDir={{ base: "column", lg: "row" }}
-            >
-              {watch("fees").map((service) => (
-                <Card.Root flexGrow={1}>
-                  <Card.Body>
-                    <Flex
-                      justifyContent={"space-between"}
-                      alignItems={"center"}
-                      gapX={4}
-                    >
-                      <Text fontSize={"lg"} fontWeight={"medium"}>
-                        {service.type}
-                      </Text>
-
-                      <MenuRoot>
-                        <MenuTrigger asChild>
-                          <IconButton size={"sm"} variant={"outline"}>
-                            <LuEllipsisVertical />
-                          </IconButton>
-                        </MenuTrigger>
-                        <MenuContent
-                          position={"absolute"}
-                          top={"68px"}
-                          right={0}
-                        >
-                          <MenuItem
-                            value="new-txt"
-                            justifyContent={"space-between"}
-                          >
-                            Edit
-                            <LuPencil />
-                          </MenuItem>
-                          <MenuItem
-                            value="new-txt"
-                            justifyContent={"space-between"}
-                          >
-                            Delete
-                            <LuTrash2 />
-                          </MenuItem>
-                        </MenuContent>
-                      </MenuRoot>
-                    </Flex>
-                    <Text display={"flex"} alignItems={"center"}>
-                      <LuDollarSign />
-                      {Intl.NumberFormat().format(service.amount)}
-                    </Text>
-                    <Text mt={4} fontSize={"sm"}>
-                      {service.description}
-                    </Text>
-                  </Card.Body>
-                </Card.Root>
-              ))}
-            </SimpleGrid>
-          </Flex>
-        </Flex>
+        <FeesEditor control={control} />
 
         <Button onClick={handleSubmit(handleSaveBtn)}>Save</Button>
       </Fieldset.Root>
