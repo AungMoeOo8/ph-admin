5925516763d7cbed752ab8f6c03b837be2ff6159
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-12-30 23:07:03 +0630

    feat: add deleteFile function

--- src/features/wordpress/upload.service.ts
+++ src/features/wordpress/upload.service.ts
@@ -8,3 +8,14 @@ export async function uploadFile(file: File) {
 
     return data;
 }
+
+export async function deleteFile(fileUrl: string) {
+    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/delete`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url: fileUrl })
+    })
+    const data = await res.json() as { isSuccess: boolean, message: string, url: string };
+
+    return data;
+}
