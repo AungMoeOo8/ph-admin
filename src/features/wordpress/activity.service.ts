bdc10719e59032ae58c031773062a0e203610823
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-01-03 21:41:36 +0630

    fix: fix activity endpoint

--- src/features/wordpress/activity.service.ts
+++ src/features/wordpress/activity.service.ts
@@ -8,21 +8,21 @@ export type ActivityProps = {
 }
 
 export async function getActivities() {
-    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/course`)
+    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/activity`)
     const data = await res.json() as { isSuccess: boolean, message: string, data: ActivityProps[] };
 
     return data
 }
 
 export async function getActivityById(id: string) {
-    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/course/${id}`)
+    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/activity/${id}`)
     const data = await res.json() as { isSuccess: boolean, message: string, data: ActivityProps };
 
     return data
 }
 
 export async function createActivity(service: ActivityProps) {
-    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/course`, {
+    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/activity`, {
         method: "POST",
         body: JSON.stringify(service),
         headers: { "Content-Type": "application/json" }
@@ -33,7 +33,7 @@ export async function createActivity(service: ActivityProps) {
 }
 
 export async function updateActivity(id: string, service: ActivityProps) {
-    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/course/${id}`, {
+    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/activity/${id}`, {
         method: "PATCH",
         body: JSON.stringify(service),
         headers: { "Content-Type": "application/json" }
@@ -44,7 +44,7 @@ export async function updateActivity(id: string, service: ActivityProps) {
 }
 
 export async function deleteActivity(id: string) {
-    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/course/${id}`, { method: "DELETE" })
+    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/activity/${id}`, { method: "DELETE" })
     const data = await res.json() as { isSuccess: boolean, message: string, data: ActivityProps };
 
     return data;
