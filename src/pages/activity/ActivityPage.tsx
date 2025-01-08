057ecbb73fa45ee430061504fb76ac69998a45ed
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-01-08 20:45:51 +0630

    fix: fix error

--- src/pages/activity/ActivityPage.tsx
+++ src/pages/activity/ActivityPage.tsx
@@ -1,45 +1,34 @@
-import { toaster } from "@/components/ui/toaster";
 import {
-  ActivityProps,
-  getActivities,
-} from "@/features/wordpress/activity.service";
-import {
-  deletePerson,
-} from "@/features/wordpress/people.service";
-import {
-  Box,
   Button,
   Flex,
-  Image,
   SimpleGrid,
   Stack,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { LuPencil, LuPlus, LuTrash } from "react-icons/lu";
+import { LuPlus } from "react-icons/lu";
 import { Link } from "react-router";
 
 export default function ActivityPage() {
-  const [activityList, setActivityList] = useState<ActivityProps[]>([]);
+  // const [activityList, setActivityList] = useState<ActivityProps[]>([]);
 
-  useEffect(() => {
-    (async () => {
-      const response = await getActivities();
-      const activities = response.data;
-      setActivityList(activities);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await getActivities();
+  //     const activities = response.data;
+  //     setActivityList(activities);
+  //   })();
+  // }, []);
 
-  async function handleDeleteBtn(id: string) {
-    const response = await deletePerson(id);
-    toaster.create({
-      type: "success",
-      description: "Deleting successful.",
-    });
+  // async function handleDeleteBtn(id: string) {
+  //   const response = await deletePerson(id);
+  //   toaster.create({
+  //     type: "success",
+  //     description: "Deleting successful.",
+  //   });
 
-    if (response.isSuccess) {
-      setActivityList(activityList.filter((activity) => activity.id != id));
-    }
-  }
+  //   if (response.isSuccess) {
+  //     setActivityList(activityList.filter((activity) => activity.id != id));
+  //   }
+  // }
 
   return (
     <Stack gap="10" w={"full"}>
@@ -51,11 +40,11 @@ export default function ActivityPage() {
         </Button>
       </Flex>
       <SimpleGrid>
-        {activityList.map((activity) => (
+        {/* {activityList.map((activity) => (
           <Box key={activity.id}>
             <Image src={activity.url} />
           </Box>
-        ))}
+        ))} */}
       </SimpleGrid>
     </Stack>
   );
