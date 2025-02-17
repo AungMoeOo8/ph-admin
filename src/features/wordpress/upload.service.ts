2e0189b45e8ac1bc59923b9ec8b96020a63a63f5
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-02-17 19:56:28 +0630

    feat: add wordpress updateFile service

--- src/features/wordpress/upload.service.ts
+++ src/features/wordpress/upload.service.ts
@@ -1,6 +1,15 @@
 const { VITE_WORDPRESS_DOMAIN } = import.meta.env
 
-export async function uploadFile(file: File) {
+export async function uploadFile(filePath: string, file: File) {
+    const body = new FormData();
+    body.append("image", file)
+    const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/upload`, { method: "POST", body: body })
+    const data = await res.json() as { isSuccess: boolean, message: string, url: string };
+
+    return data;
+}
+
+export async function updateFile(filePath: string, file: File) {
     const body = new FormData();
     body.append("image", file)
     const res = await fetch(`${VITE_WORDPRESS_DOMAIN}/phweb/wp-json/api/upload`, { method: "POST", body: body })
