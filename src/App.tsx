ad5ab01696f4ed447e528ef92f71ea9fa6298e50
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-07-05 04:26:11 +0700

    fix app navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { useEffect } from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { useAuth } from "./hooks/auth";
 
 export default function App() {
   const { isAuthenticated } = useAuth();
+  const navigate = useNavigate()
 
   useEffect(() => {
-    // if (!isAuthenticated) {
-    //   navigate("/login", { replace: true, flushSync: false });
-    //   return;
-    // }
-    // if (location.pathname === "/login") {
-    //   navigate("/dashboard/people", { replace: true });
-    // }
+    if (!isAuthenticated) {
+      navigate("/login", { replace: true, flushSync: false });
+      return;
+    }
+    if (location.pathname === "/login") {
+      navigate("/dashboard/people", { replace: true });
+    }
   }, [isAuthenticated]);
 
   return <Outlet />;
